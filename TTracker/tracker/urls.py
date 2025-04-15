b348f3f76c72f75182acef66b62553cf48e3ad89
laci8 <lacivetta8@gmail.com>
2025-04-15 20:25:54 +0200

    Delete TTracker directory

--- TTracker/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('track-behavior/', views.track_behavior, name='track_behavior'),
-    path('track-end/', views.track_end, name='track_end'),
-
-]
